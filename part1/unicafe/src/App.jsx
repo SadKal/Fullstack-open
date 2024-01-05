783f46e92bbd889cfba4a93b6369936e5cdc83b4
SadKal <huertacimasa@gmail.com>
2024-01-05 16:16:01 +0100

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,12 +10,17 @@ const Button = (props) => {
 }
 
 const Display = (props) => {
-  const {good, neutral, bad} = props;
+  const {good, neutral, bad, avg} = props;
+  const total = good + neutral + bad;
+  const positive = good / total * 100
   return (
     <div>
       good {good} <br/>
       neutral {neutral} <br/>
-      bad {bad}   
+      bad {bad} <br/> 
+      all {total} <br/>
+      average {avg/total} <br/>
+      positive {positive}%
     </div>
   );
 }
@@ -25,15 +30,18 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [avg, setAvg] = useState(0)
 
   const clickGood = () => {
     setGood(good + 1)
+    setAvg(avg + 1)
   }
   const clickNeutral = () => {
     setNeutral(neutral + 1)
   }
   const clickBad = () => {
     setBad(bad + 1)
+    setAvg(avg -1)
   }
 
   return (
@@ -46,7 +54,7 @@ const App = () => {
       </div>
       
       <h1>Statistics</h1>
-      <Display good={good} neutral={neutral} bad={bad}/>
+      <Display good={good} neutral={neutral} bad={bad} avg={avg}/>
     </div>
   )
 }
